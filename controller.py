5311c2863ac30fecd9b018dfaaf9cc51e0592b59
John Alexander Cifuentes <john.alexcz@gmail.com>
2021-10-09 12:34:11 -0500

    consultas

--- controller.py
+++ controller.py
@@ -13,3 +13,22 @@ def update_customer(name,email,phone,passwd):
     cnn = get_connection()
     with cnn.cursor() as cursor:
         cursor.execute("UPDATE user SER name = %s, email = %s, phone = %s, passwd = %s WHERE id = %s, (name,email,phone,passwd,id) ")
+        cnn.commit()
+        cnn.close()
+    
+def delete_customer(id):
+    cnn = get_connection()
+    with cnn.cursor() as cursor:
+        cursor.execute("DELETE FROM user WHERE id = %s", (id))
+        cnn.commit()
+        cnn.close()
+
+def get_customer():
+    cnn = get_connection()
+    customer = []
+    with cnn.cursor() as cursor:
+        cursor.execute("SELECT id, name, phone, passwd FROM cliente")
+        customer = cursor.fetchall()
+        cnn.close()
+        return customer
+

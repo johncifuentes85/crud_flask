273b605182384a65d80036bc86334f78c26a0e7a
John Alexander Cifuentes <john.alexcz@gmail.com>
2021-10-09 14:04:54 -0500

    Initial commit

--- controller.py
+++ controller.py
@@ -2,33 +2,41 @@
 from configdb import get_connection
 
 #se crea una funcion para agregar cliente y pedimos los campos pueden llamarse igual a la variables de mysql o diferente
-def add_customer(name,email,phone,passwd):
+def add_user(name,email,phone,passwd):
     cnn = get_connection()
     with cnn.cursor() as cursor:
         cursor.execute("INSER INTO user(name,email,phone,passwd) VALUES (%s,%s,%s,%s)",(name,email,phone,passwd))
         cnn.commit()
         cnn.close()
 
-def update_customer(name,email,phone,passwd):
+def update_user(name,email,phone,passwd):
     cnn = get_connection()
     with cnn.cursor() as cursor:
         cursor.execute("UPDATE user SER name = %s, email = %s, phone = %s, passwd = %s WHERE id = %s, (name,email,phone,passwd,id) ")
         cnn.commit()
         cnn.close()
     
-def delete_customer(id):
+def delete_user(id):
     cnn = get_connection()
     with cnn.cursor() as cursor:
         cursor.execute("DELETE FROM user WHERE id = %s", (id))
         cnn.commit()
         cnn.close()
 
-def get_customer():
+def get_user():
     cnn = get_connection()
-    customer = []
+    user = []
     with cnn.cursor() as cursor:
-        cursor.execute("SELECT id, name, phone, passwd FROM cliente")
-        customer = cursor.fetchall()
+        cursor.execute("SELECT id, name, email, phone FROM cliente")
+        user = cursor.fetchall()
         cnn.close()
-        return customer
+        return user
+
+def get_user_id(id):
+    cnn = get_connection()
+    user = None
+    with cnn.cursor() as cursor:
+        cursor.execute("SELECT id, name, phone, passwd FROM cliente WHERE id = %s", (id))
+    cnn.close()
+    return user
 
